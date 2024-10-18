46964d9c77a8cbd90a139b2fad45a953bc3ed745
Edwin Moreno <edwinmoreno77@gmail.com>
2024-10-18 20:40:45 -0300

    merge createUser with develop

--- app.py
+++ app.py
@@ -23,7 +23,6 @@ cloudinary.config(
 
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mibasededatos.db'
 app.config["JWT_SECRET_KEY"] = os.getenv('JWT_SECRET_KEY')
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
